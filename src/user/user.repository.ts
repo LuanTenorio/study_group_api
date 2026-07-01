bc37879c28bd3090a3ab283076561519811173ee
João Victor <joaovictorsilvafreitas@hotmail.com>
2026-07-01 19:14:50 -0300

    fix: add promise on findByEmail

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -7,7 +7,7 @@ import { UserQuery } from './query.enum';
 export class UserRepository {
     constructor(@Inject('DATABASE_POOL') private pool: Pool) {}
 
-    async findByEmail(email: string) {
+    async findByEmail(email: string): Promise<UserPgDto | undefined> {
         const user = await this.pool.query<UserPgDto>(UserQuery.SELECT_BY_EMAIL, [email])
 
         return user.rows[0];
