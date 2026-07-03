bf6f49027c6f50e2cbb6572aa83754172cee919b
caue-oliver <caue.ooliver@gmail.com>
2026-07-03 16:43:23 -0300

    update: userpgDTO

--- src/user/dto/user_pg.dto.ts
+++ src/user/dto/user_pg.dto.ts
@@ -2,11 +2,10 @@ import { UserDto } from "src/user/dto/user.dto";
 
 export class UserPGDto {
 
-
     id: string;
     name: string;
     email: string;
     institution_id: number;
-    hashedpassowrd: string;
+    hashedpassword: string;
 
 }
