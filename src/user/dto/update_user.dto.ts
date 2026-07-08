4b070c7ef84dedab4b8d17741dcbafa65fa2d9f0
João Victor <joaovictorsilvafreitas@hotmail.com>
2026-07-08 11:24:14 -0300

    hot-fix: atualização de perfil

--- src/user/dto/update_user.dto.ts
+++ src/user/dto/update_user.dto.ts
@@ -1,5 +1,16 @@
+import { Type } from 'class-transformer';
+import { IsEmail, IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
+
 export class UpdateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
     institution_id: number;
 }
